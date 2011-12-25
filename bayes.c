7f8c33242041636c035c5132f6582fec187748d8
Fernando Izquierdo <fer.izquierdo@gmail.com>
2011-12-25 19:45:41 +0100

    Added NNI moves (so far w/o bl changes) as alternative to SPR

--- bayes.c
+++ bayes.c
@@ -31,6 +31,7 @@ extern partitionLengths pLengths[MAX_MODEL];
 typedef enum
 {
   SPR,
+  stNNI,
   UPDATE_ALL_BL,
   UPDATE_MODEL,
   UPDATE_GAMMA
@@ -57,6 +58,10 @@ typedef struct {
   double nnbz[NUM_BRANCHES];
   nodeptr r; /* edge neighbour of re-insertion node, q->back */
   nodeptr q; /* re-insertion node */
+  /*
+   * these are the bits that are necessary for the NNI moves
+   */
+  int whichNNI; /* 0 same topo, 1, 2 */
 
   /*
    * necessary for the branch length moves
@@ -188,7 +193,6 @@ static double exp_pdf(double lambda, double x)
   return lambda * exp(-(lambda * x)); 
 }
 
-
 static void printSimpleTree(tree *tr, boolean printBranchLengths, analdef *adef)
 {
   Tree2String(tr->tree_string, tr, tr->start->back, printBranchLengths, 1, 0, 0, 0, SUMMARIZE_LH, 0,0);
@@ -239,27 +243,6 @@ static void nodeVisitor(nodeptr p, int numtips, int radius, int maxradius, nodep
   return;
 }
 
-/*
-static void showTreeNodes(tree *tr)
-{
-  int i;
-  nodeptr p, q;
-  printBothOpen("tree map\n");
-  for(i = tr->mxtips + 1; i <= tr->mxtips + tr->mxtips - 2; i++)
-  {
-    q = tr->nodep[i];
-    p = q; 
-    while(!p->x)
-      p = p->next;
-    printBothOpen("node %d b %d, nb %d nnb %d\n",  
-        p->number, 
-        p->back->number,  
-        p->next->back->number,  
-        p->next->next->back->number);
-  }
-  printBothOpen("\n");
-}
-*/
 
 static void naiveInsertionProposal(state *s)
 {
@@ -362,6 +345,125 @@ static void reset_branch_length(nodeptr p, int numBranches)
     p->z[i] = p->back->z[i] = p->z_tmp[i];   /* restore saved value */
   }
 }
+
+
+/* start NNI topologycal prop */
+static void recordNNIBranchInfo(nodeptr p, int numBranches)
+{
+  int i;
+  nodeptr q = p->back;
+  for(i = 0; i < numBranches; i++)
+  {
+    p->z_tmp[i] =  p->back->z_tmp[i] = p->z[i];
+    p->next->z_tmp[i] =  p->next->back->z_tmp[i] = p->next->z[i];
+    p->next->next->z_tmp[i] = p->next->next->back->z_tmp[i] = p->next->next->z[i];
+    q->next->z_tmp[i] = q->next->back->z_tmp[i] = q->next->z[i];
+    q->next->next->z_tmp[i] = q->next->next->back->z_tmp[i] = q->next->next->z[i];
+  }
+}
+static showNNI_move(nodeptr p)
+{
+  printBothOpen("new NNI from p %d, pnb %d, pnnb %d \n", p->number, 
+      p->next->back->number,  p->next->next->back->number);
+  nodeptr q = p->back;
+  printBothOpen("new NNI to q %d, qnb %d, qnnb %d \n", q->number, 
+      q->next->back->number,  q->next->next->back->number);
+}
+static boolean stNNIproposal(state *s)
+{
+  s->newprior = 1;
+  int attempts = 0;
+  do{
+    s->p = selectRandomInnerSubtree(s->tr); /* TODOFER do this ad hoc for NNI requirements*/
+    if (++attempts > 500)
+      return FALSE;
+  }while(isTip(s->p->number, s->tr->mxtips) || isTip(s->p->back->number, s->tr->mxtips));
+  assert(!isTip(s->p->number, s->tr->mxtips));
+  nodeptr 
+    p = s->p,
+    q = s->p->back,
+    pb1 = s->p->next->back,
+    pb2 = s->p->next->next->back;
+  assert(!isTip(q->number, s->tr->mxtips));
+  nodeptr
+    qb1 = q->next->back,
+    qb2 = q->next->next->back;
+
+  recordNNIBranchInfo(p, s->tr->numBranches);
+  /* do only one type of NNI, nni1 */
+  double randprop = (double)rand()/(double)RAND_MAX;
+  if (randprop < 1.0 / 3.0)
+  {
+    s->whichNNI = 1;
+    hookup(p, q, p->z, s->tr->numBranches);
+    hookup(p->next,       qb1, q->next->z, s->tr->numBranches);
+    hookup(p->next->next, pb2, p->next->next->z, s->tr->numBranches);
+    hookup(q->next,       pb1, p->next->z, s->tr->numBranches);
+    hookup(q->next->next, qb2, q->next->next->z, s->tr->numBranches);
+  }
+  else if (randprop < 2.0 / 3.0)
+  {
+    s->whichNNI = 2;
+    hookup(p, q, p->z, s->tr->numBranches);
+    hookup(p->next,       pb1, p->next->z, s->tr->numBranches);
+    hookup(p->next->next, qb1, q->next->z, s->tr->numBranches);
+    hookup(q->next,       pb2, p->next->next->z, s->tr->numBranches);
+    hookup(q->next->next, qb2, q->next->next->z, s->tr->numBranches);
+  }
+  else
+  {
+    /* change only the branch lengths */
+    s->whichNNI = 0; 
+  }
+
+  newviewGeneric(s->tr, p, FALSE);
+  newviewGeneric(s->tr, p->back, FALSE);
+  evaluateGeneric(s->tr, p, FALSE);
+  return TRUE;
+}
+reset_stNNI(state *s)
+{
+  nodeptr p, q;
+  p = s->p;
+  q = p->back;
+  nodeptr pb1, pb2;
+  pb1 = p->next->back;
+  pb2 = p->next->next->back;
+  nodeptr qb1, qb2;
+  qb1 = q->next->back;
+  qb2 = q->next->next->back;
+
+  /* whichNNI 1*/
+  if(s->whichNNI == 1)
+  {
+    hookup(p, q, p->z_tmp, s->tr->numBranches);
+    hookup(p->next,       qb1, p->next->z_tmp, s->tr->numBranches);
+    hookup(p->next->next, pb2, p->next->next->z_tmp, s->tr->numBranches);
+    hookup(q->next,       pb1, q->next->z_tmp, s->tr->numBranches);
+    hookup(q->next->next, qb2, q->next->next->z_tmp, s->tr->numBranches);
+  }
+  else if(s->whichNNI == 2)
+  {
+    hookup(p, q, p->z_tmp, s->tr->numBranches);
+    hookup(p->next,       pb1, p->next->z_tmp, s->tr->numBranches);
+    hookup(p->next->next, qb1, p->next->next->z_tmp, s->tr->numBranches);
+    hookup(q->next,       pb2, q->next->z_tmp, s->tr->numBranches);
+    hookup(q->next->next, qb2, q->next->next->z_tmp, s->tr->numBranches);
+  }
+
+  /*
+  reset_branch_length(p, s->tr->numBranches);
+  reset_branch_length(p->next, s->tr->numBranches);
+  reset_branch_length(p->next->next, s->tr->numBranches);
+  reset_branch_length(q->next, s->tr->numBranches);
+  reset_branch_length(q->next->next, s->tr->numBranches);
+  */
+
+  newviewGeneric(s->tr, p, FALSE);
+  newviewGeneric(s->tr, q, FALSE);
+}
+
+/* end NNI topologycal prop */
 
 //setting this out to allow for other types of setting
 static void set_branch_length_sliding_window(nodeptr p, int numBranches,state * s)
@@ -604,21 +706,38 @@ static prop proposal(state * instate)
   {
     if(randprop < 0.2)//SPR MOVE
     {
-      proposal_type = SPR;
-      // printBothOpen("Propose SPR\n");
-      if (randprop < 0.15)
-	instate->maxradius = 1;
-      else
-	instate->maxradius = 2;
-      proposalSuccess = simpleNodeProposal(instate);
-      if(proposalSuccess == FALSE)
+      if(randprop > 0.1)//SPR MOVE
       {
-	assert(FALSE); // this should either never happen or look below and return PROPOSAL_FAILED to react accordingly
+        proposal_type = SPR;
+        // printBothOpen("Propose SPR\n");
+        if (randprop < 0.15)
+          instate->maxradius = 1;
+        else
+          instate->maxradius = 2;
+        proposalSuccess = simpleNodeProposal(instate);
       }
       else
       {
-	/* A moved has been made, previous state is in instate */
-	assert(instate->tr->startLH != instate->tr->likelihood);
+        proposal_type = stNNI;
+        proposalSuccess = stNNIproposal(instate); 
+        if(proposalSuccess == FALSE)
+        {
+          /* TODOFER this came up with ds 20 and GTRPSR, see why */
+          printBothOpen("WARNING!! stNNI proposal failed, doing SPR\n");
+          proposal_type = SPR;
+          instate->maxradius = 1;
+          proposalSuccess = simpleNodeProposal(instate);
+        }
+      }
+      if(proposalSuccess == FALSE)
+      {
+        assert(FALSE); // this should either never happen or look below and return PROPOSAL_FAILED to react accordingly
+      }
+      else
+      {
+        /* A moved has been made, previous state is in instate */
+        if(proposal_type != stNNI) /*TODOFER delete this when bl are changed*/
+          assert(instate->tr->startLH != instate->tr->likelihood);
       }
     }
     else{//MODEL
@@ -655,6 +774,9 @@ static void resetState(prop proposal_type, state * curstate)
   {
     case SPR:
       resetSimpleNodeProposal(curstate);
+      break;
+    case stNNI:
+      reset_stNNI(curstate);
       break;
     case UPDATE_ALL_BL:
       resetSimpleBranchLengthProposal(curstate);
@@ -713,8 +835,8 @@ void mcmc(tree *tr, analdef *adef)
   int j;
 
   int maxradius = 30;
-  int accepted_spr = 0, accepted_bl = 0, accepted_model = 0, accepted_gamma = 0, inserts = 0;
-  int rejected_spr = 0, rejected_bl = 0, rejected_model = 0, rejected_gamma = 0;
+  int accepted_spr = 0, accepted_nni = 0, accepted_bl = 0, accepted_model = 0, accepted_gamma = 0, inserts = 0;
+  int rejected_spr = 0, rejected_nni = 0, rejected_bl = 0, rejected_model = 0, rejected_gamma = 0;
   int num_moves = 10000;
   boolean proposalAccepted;
   boolean proposalSuccess;
@@ -772,14 +894,16 @@ void mcmc(tree *tr, analdef *adef)
     */
 
     which_proposal = proposal(curstate);
+    //which_proposal = stNNIproposal(curstate); /* force tmp for testing */
     if (first == 1)
     {
       first = 0;
       curstate->curprior = curstate->newprior;
     }
    //printBothOpen("proposal done, iter %d tr LH %f, startLH %f\n", j, tr->likelihood, tr->startLH);
-    assert(which_proposal == SPR || which_proposal == UPDATE_ALL_BL || which_proposal == UPDATE_MODEL
-      || which_proposal == UPDATE_GAMMA);
+    assert(which_proposal == SPR || which_proposal == stNNI ||
+           which_proposal == UPDATE_ALL_BL || 
+           which_proposal == UPDATE_MODEL || which_proposal == UPDATE_GAMMA);
     proposalTime += gettime() - t;
     /* decide upon acceptance */
     testr = (double)rand()/(double)RAND_MAX;
@@ -815,6 +939,8 @@ void mcmc(tree *tr, analdef *adef)
       curstate->curprior = curstate->newprior;
       if(which_proposal == SPR)
         accepted_spr++;
+      else if (which_proposal == stNNI)
+        accepted_nni++;
       else if (which_proposal == UPDATE_ALL_BL)
         accepted_bl++;
       else if (which_proposal == UPDATE_MODEL)
@@ -828,6 +954,8 @@ void mcmc(tree *tr, analdef *adef)
       resetState(which_proposal,curstate);
       if(which_proposal == SPR)
         rejected_spr++;
+      else if (which_proposal == stNNI)
+        rejected_nni++;
       else if (which_proposal == UPDATE_ALL_BL)
         rejected_bl++;
       else if (which_proposal == UPDATE_MODEL)
@@ -862,12 +990,12 @@ void mcmc(tree *tr, analdef *adef)
   t = gettime(); 
   treeEvaluate(tr, 1);
   blTime += gettime() - t;
-  printBothOpen("accepted SPR %d, accepted BL %d, accepted model %d, accepted gamma %d, num moves tried %d, SPRs with max radius %d\n", 
-		accepted_spr, accepted_bl, accepted_model, accepted_gamma, num_moves, maxradius);
-  printBothOpen("rejected SPR %d, rejected BL %d, rejected model %d, rejected gamma %d\n",
-		rejected_spr, rejected_bl, rejected_model, rejected_gamma);
-  printBothOpen("ratio SPR %f, ratio BL %f, ratio model %f, ratio gamma %f\n",
-		accepted_spr/(double)(rejected_spr+accepted_spr), accepted_bl/(double)(rejected_bl+accepted_bl), 
+  printBothOpen("accepted SPR %d, accepted stNNI %d, accepted BL %d, accepted model %d, accepted gamma %d, num moves tried %d, SPRs with max radius %d\n", 
+		accepted_spr, accepted_nni, accepted_bl, accepted_model, accepted_gamma, num_moves, maxradius);
+  printBothOpen("rejected SPR %d, rejected stNNI %d, rejected BL %d, rejected model %d, rejected gamma %d\n",
+		rejected_spr, rejected_nni, rejected_bl, rejected_model, rejected_gamma);
+  printBothOpen("ratio SPR %f, ratio stNNI %f,  ratio BL %f, ratio model %f, ratio gamma %f\n",
+		accepted_spr/(double)(rejected_spr+accepted_spr), accepted_nni/(double)(rejected_nni+accepted_nni), accepted_bl/(double)(rejected_bl+accepted_bl), 
 		accepted_model/(double)(rejected_model+accepted_model), accepted_gamma/(double)(rejected_gamma+accepted_gamma));
   printBothOpen("total  %f, BL %f, printing %f, proposal %f\n", gettime()- t_start, blTime, printTime, proposalTime);
   assert(inserts == num_moves);
