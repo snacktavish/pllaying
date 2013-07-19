d4fc02f4d3ee3d58e6a35b3c33277cfefd43a7a6
Tomas Flouri <xflouris@gmail.com>
2013-07-19 11:17:20 +0200

    added missing include file

--- axml.h
+++ axml.h
@@ -66,6 +66,7 @@ extern "C" {
 
 #include "genericParallelization.h"
 #include "mem_alloc.h"
+#include "errcodes.h"
 
 #define PLL_MAX_TIP_EV                          0.999999999 /* max tip vector value, sum of EVs needs to be smaller than 1.0, otherwise the numerics break down */
 #define PLL_MAX_LOCAL_SMOOTHING_ITERATIONS      32          /** @brief maximum iterations of smoothings per insert in the */
