26aa9c57b6ad83e02fe45ffb3425058cd5a8769d
Tomas Flouri <xflouris@gmail.com>
2013-08-06 18:39:41 +0200

    Fixed bug in core pinning. POSIX standard does not guarantee that thread IDs start from 1 (i.e. the version on my laptop:) )

--- genericParallelization.c
+++ genericParallelization.c
@@ -173,10 +173,12 @@ void defineTraversalInfoMPI()
  */ 
 void pinToCore(int tid)
 {
+  static int nextCore = 0;
+
   cpu_set_t cpuset;
 
   CPU_ZERO(&cpuset);    
-  CPU_SET(tid, &cpuset);
+  CPU_SET(nextCore++, &cpuset);
 
   if(pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) != 0)
     {
