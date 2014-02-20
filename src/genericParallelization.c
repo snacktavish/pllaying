bbc93e04f79f634764db6cdc370ec487656c5efc
Tomas Flouri <flouris@gmail.com>
2014-02-20 09:58:56 +0100

    added missing include file

--- src/genericParallelization.c
+++ src/genericParallelization.c
@@ -35,8 +35,9 @@
 
 #include <assert.h>
 
-#include "pll.h"
+#include "genericParallelization.h"
 #include "pllInternal.h"
+#include "pll.h"
 
 /** @file genericParallelization.c
     
@@ -731,7 +732,7 @@ static void multiprocessorScheduling(pllInstance * tr, partitionList *pr, int ti
     
     We collect the first and second derivatives from the various
     threads and sum them up. It's similar to what we do in
-    pllEvaluateLikelihood() with the only difference that we have to collect
+    pllEvaluateGeneric() with the only difference that we have to collect
     two values (firsrt and second derivative) instead of onyly one (the
     log likelihood
 
